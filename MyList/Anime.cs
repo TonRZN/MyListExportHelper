d6fe3f53dad3269d66dd2000d13e281e8b496ddb
TonRZN <TonRZN@users.noreply.github.com>
2019-02-02 20:03:48 -0200

    Add project files.

---
+++ MyList/Anime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    public class Anime
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string EpisodeNumber { get; set; }
+        public string EpisodeTitle { get; set; }
+        public string GroupName { get; set; }
+        public string ED2K { get; set; }
+
+        public bool isComplete()
+        {
+            return !String.IsNullOrEmpty(ID) && !String.IsNullOrEmpty(Name) && !String.IsNullOrEmpty(EpisodeNumber) && !String.IsNullOrEmpty(EpisodeTitle) && !String.IsNullOrEmpty(GroupName) && !String.IsNullOrEmpty(ED2K);
+        }
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MyList
+{
+    internal class Program
+    {
+        private static readonly Regex title = new Regex(@"# File: (?<AnimeName>.*) - (?<EpisodeNumber>.*) - (?<EpisodeName>.*) - (?<GroupName>.*).(?<FileExtension>.{3,})");
+        private static readonly Regex ed2k = new Regex(@"# UID: (?<FileSize>[0-9]+):(?<ED2K>.+)");
+        private static readonly Regex mylist = new Regex(@"(221 MYLIST)+");
+        private static readonly Regex aid = new Regex(@"(\|+\d*)+");
+
+        private static List<Anime> animes = new List<Anime>();
+
+        private static void Main(string[] args)
+        {
+            //Code to handling command line launch
+            string path = "";
+            if (args.Length > 0)
+                path = args[0];
+
+            if (File.Exists(path))
+                createCleanJson(path);
+            else
+                Console.WriteLine("File not found");
+
+            //Command to handle manual launch
+            Console.WriteLine("Insert file path:");
+            Console.Write(">");
+            createCleanJson(Console.ReadLine());
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static void createCleanJson(string file)
+        {
+            Anime current = new Anime();
+
+            var lines = File.ReadLines(file);
+            foreach (var line in lines)
+            {
+                //Tries to get the title part of the export => # File: ED2K file name
+                var matchTitle = title.Match(line);
+                if (matchTitle.Length > 0)
+                {
+                    try
+                    {
+                        current.Name = matchTitle.Groups["AnimeName"].ToString();
+                        current.EpisodeNumber = matchTitle.Groups["EpisodeNumber"].ToString();
+                        current.EpisodeTitle = matchTitle.Groups["EpisodeName"].ToString();
+                        current.GroupName = matchTitle.Groups["GroupName"].ToString();
+                    }
+                    catch { }
+                }
+
+                //Tries to get the  ED2K part of the export => # UID: file size in bytes:ED2K hash
+                var matchEd2k = ed2k.Match(line);
+                if (matchEd2k.Length > 0)
+                    try { current.ED2K = matchEd2k.Groups["ED2K"].ToString(); } catch { }
+
+                //Tries to get the mylist UDP success code => 221 MYLIST
+                var matchMylist = mylist.Match(line);
+                if (matchMylist.Length > 0)
+                    try { Console.WriteLine("MyList code ignored"); } catch { } //Just to see if this is working
+
+                //Tries to get the anime id from the string => {int4 lid}|{int4 fid}|{int4 eid}|{int4 aid}|{int4 gid}|{int4 date}|{int2 state}|{int4 viewdate}|{str storage}|{str source}|{str other}|{int2 filestate} 
+                var matchAid = aid.Match(line);
+                if (matchAid.Length > 0)
+                    try { current.ID = matchAid.Value.Split('|')[3].ToString(); } catch { /*TODO : A better regex pattern...*/ }
+
+                //If all fields are filled then procced to next anime
+                if (current.isComplete())
+                {
+                    animes.Add(current);
+                    current = new Anime();
+                }
+            }
+
+            //Write the new file
+            File.WriteAllText(Path.ChangeExtension(file, "_clean.json"), JsonConvert.SerializeObject(animes));
+        }
+    }
+}
